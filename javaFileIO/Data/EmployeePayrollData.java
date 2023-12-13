f114375f5ce52bde40c797f029430e5736d05035
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-14 02:25:41 +0530

    Added: method to write employeePayroll into file and count total no. of entries

--- javaFileIO/Data/EmployeePayrollData.java
+++ javaFileIO/Data/EmployeePayrollData.java
@@ -1,11 +1,26 @@
 package org.bridgeLabz.javaFileIO.Data;
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class EmployeePayrollData {
     private int id;
     private String EmpName;
     private long salary;
+    public EmployeePayrollData(){
+        id=0;
+        EmpName=null;
+        salary=0;
+    }
+    public EmployeePayrollData(int id,String EmpName,long salary){
+        this.id=id;
+        this.EmpName=EmpName;
+        this.salary=salary;
+    }
    // getter fnc
     public int getId() {
         return id;
@@ -28,9 +43,10 @@ public class EmployeePayrollData {
     }
 /*
 @desc: read data from console
+@return: EmployeePayrollData with instance variable as values given by user
 @throws IO Exception
  */
-    public void readFromConsole() throws IOException {
+    public EmployeePayrollData readFromConsole() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the Id");
         id = Integer.parseInt(reader.readLine());
@@ -38,5 +54,32 @@ public class EmployeePayrollData {
         EmpName= reader.readLine();
         System.out.println("Enter the salary");
         salary=Long.parseLong(reader.readLine());
+        EmployeePayrollData emp= new EmployeePayrollData(id,EmpName,salary);
+        return emp;
+    }
+    /*
+    @desc: write employeePayroll Object instance variables into file which have path as
+           pathName
+    @params: EmployeePayrollData object and String as pathName
+    @throws: IOException
+     */
+    public void WriteinFile(EmployeePayrollData emp, String pathName) throws IOException{
+
+        FileWriter writer= new FileWriter(pathName);
+        writer.write("id: " + emp.getId());
+        writer.write("name: "+emp.getEmpName());
+        writer.write("salary: "+emp.getSalary());
+    }
+    /*
+    @desc:compute the total no. of entries of any directory
+    @params: filePath as String
+    @return: no. of entries
+    @throws:IOException
+    */
+    public int  countEntry(String filePath) throws IOException{
+        Path path= Paths.get(filePath);
+        int cntEntry= (int) Files.lines(path).count();
+        System.out.println("Number of Entries are: "+cntEntry);
+        return cntEntry;
     }
 }
--- javaFileIO/service/EmployeeService.java
+++ javaFileIO/service/EmployeeService.java
@@ -10,14 +10,21 @@ public class EmployeeService {
         System.out.println("Employee Payroll Service");
         EmployeePayrollData emp= new EmployeePayrollData();
         try {
-            emp.readFromConsole();
+            EmployeePayrollData emp2=emp.readFromConsole();
             System.out.println(emp.getEmpName());
             System.out.println(emp.getId());
             System.out.println(emp.getSalary());
+            System.out.println(emp2.getEmpName()+" "+emp2.getId()+" "+emp2.getSalary());
+            //writing to file
+            String path="src/main/java/org/bridgeLabz/OutputFile.txt";
+            emp.WriteinFile(emp2,path);
+            // total entries of any directory
+            emp.countEntry(path);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
+
+
+
 }
